99dae54205e4a2e932b676e512355c8d4c4ccc2f
Alexander Lim <alexander.halim+github@gmail.com>
2017-07-18 16:58:36 +0800

    Enable cf-list-asg-all.sh script to output summary of ASG rules in a semicolon separated values.

--- cf-list-asg-all.sh
+++ cf-list-asg-all.sh
@@ -21,10 +21,17 @@ fi
 
 output_file="$1"
 results_per_page=100
+json_file=$(mktemp)
+echo "asg_name,running_default,staging_default,protocol,ports,destination,description" >> ${output_file}
 
 # Capture the output of security_groups
 next_url="/v2/security_groups?results-per-page=${results_per_page}"
 while [[ "${next_url}" != "null" ]]; do
-  cf curl ${next_url} >> ${output_file}
+  cf curl ${next_url} >> ${json_file}
   next_url=$(cf curl ${next_url} | jq -r -c ".next_url")
 done
+
+# Summarize ASGs in output_file separated by semicolon
+asg_rules=$(jq -r '.resources[] | .entity.name + ";" + (.entity.running_default | tostring) + ";" + (.entity.staging_default | tostring) + ";" + (.entity.rules[] | .protocol + ";" + .ports + ";"  + .destination + ";" + .description)' ${json_file})
+
+echo "$asg_rules" >> ${output_file}
