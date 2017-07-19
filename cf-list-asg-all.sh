5a312c4577762389b632b3acc1bf6ababe8eca38
Alexander Lim <alexander.halim+github@gmail.com>
2017-07-19 17:26:42 +0800

    Change output of cf-list-asg-all.sh to json and include space names that are using each individual ASG.

--- cf-list-asg-all.sh
+++ cf-list-asg-all.sh
@@ -21,17 +21,30 @@ fi
 
 output_file="$1"
 results_per_page=100
-json_file=$(mktemp)
-echo "asg_name;running_default;staging_default;protocol;ports;destination;description" >> ${output_file}
+json_file1=$(mktemp)
+json_file2=$(mktemp)
 
 # Capture the output of security_groups
 next_url="/v2/security_groups?results-per-page=${results_per_page}"
 while [[ "${next_url}" != "null" ]]; do
-  cf curl ${next_url} >> ${json_file}
+  cf curl ${next_url} >> ${json_file1}
   next_url=$(cf curl ${next_url} | jq -r -c ".next_url")
 done
 
 # Summarize ASGs in output_file separated by semicolon
-asg_rules=$(jq -r '.resources[] | .entity.name + ";" + (.entity.running_default | tostring) + ";" + (.entity.staging_default | tostring) + ";" + (.entity.rules[] | .protocol + ";" + .ports + ";"  + .destination + ";" + .description)' ${json_file})
+jq '.resources[] | {asg_name: .entity.name, spaces: .entity.spaces_url, rules: .entity.rules} | {asg_name: .asg_name, spaces: .spaces, rules: [.rules[] | .protocol + " " + .ports + " to " + .destination]} | {asg_name: .asg_name, spaces: .spaces, rules: .rules|join("; ")} ' ${json_file1} \
+>> ${json_file2}
 
-echo "$asg_rules" >> ${output_file}
+for url in $(jq -r '.spaces' ${json_file2}); do
+  org_space_set=
+  for space_guid in $(cf curl $url | jq -r '.resources[] | .metadata.guid'); do
+    space_name=$(cf curl $url | jq -r --arg value $space_guid '.resources[] | select(.metadata.guid == $value) | .entity.name')
+    org_url=$(cf curl $url | jq -r --arg value $space_guid '.resources[] | select(.metadata.guid == $value) | .entity.organization_url')
+    org_name=$(cf curl $org_url | jq -r '.entity.name')
+    org_space_set+="$org_name org"
+    org_space_set+="/"
+    org_space_set+="$space_name space"
+    org_space_set+="; "
+  done
+  jq -r --arg url "$url" --arg replacement "$org_space_set" 'select(.spaces == $url) | .spaces |= $replacement' ${json_file2} >> ${output_file}
+done
